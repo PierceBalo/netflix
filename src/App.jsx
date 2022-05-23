6ba4936920994b83ec37fa0b562a92adab4a87d0
InventedMost <netvid00@gmail.com>
2022-05-23 12:38:57 +0100

    initial commit netflix

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./app.scss"
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/watch' element={<Watch/>}/>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
